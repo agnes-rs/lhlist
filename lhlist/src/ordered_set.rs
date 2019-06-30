1bb3fbe5ba0dcb212ce9a573ced95853ce975ed2
LukeMathWalker <lpalmieri@truelayer.com>
2019-06-30 12:22:22 +0100

    Arbitrary length admitted

--- lhlist/src/ordered_set.rs
+++ lhlist/src/ordered_set.rs
@@ -3,10 +3,13 @@ use crate::{Cons, LabeledValue, Nil};
 
 impl OrderedHSet for Nil {}
 
-impl<H: Label> OrderedHSet for Cons<LabeledValue<H>, Nil> {}
+impl<H: Label, T: OrderedHSet> OrderedHSet for Cons<LabeledValue<H>, T> {}
 
 pub trait OrderedHSet: Sized {
-    fn prepend<H: Label>(self, h: LabeledValue<H>) -> Cons<LabeledValue<H>, Self> {
+    fn prepend<H>(self, h: LabeledValue<H>) -> Cons<LabeledValue<H>, Self>
+    where
+        H: Label,
+    {
         Cons {
             head: h,
             tail: self,
@@ -16,17 +19,20 @@ pub trait OrderedHSet: Sized {
 
 #[cfg(test)]
 mod tests {
-    use crate::*;
     use crate::ordered_set::OrderedHSet;
+    use crate::*;
 
     #[test]
     fn create_ordered_set() {
         #[label(type=String, crate=crate)]
         struct ProductName;
 
-        let nil = Nil {} ;
+        let nil = Nil {};
         let elem = LabeledValue::<ProductName>::new("Shampoo".to_string());
         // This should not compile once we are done
-        let ordered_set = nil.prepend(elem.clone()).prepend(elem);
+        let _ordered_set = nil
+            .prepend(elem.clone())
+            .prepend(elem.clone())
+            .prepend(elem);
     }
 }
