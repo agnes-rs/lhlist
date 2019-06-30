6e30bf9df95226b94436108c777dc86a95b8d3f2
LukeMathWalker <lpalmieri@truelayer.com>
2019-06-30 13:26:38 +0100

    Basic union implementation, missing recursion

--- lhlist/src/ordered_set.rs
+++ lhlist/src/ordered_set.rs
@@ -18,9 +18,64 @@ pub trait OrderedHSet: Sized {
     }
 }
 
+pub trait Union<Rhs: OrderedHSet> {
+    type Output: OrderedHSet;
+
+    fn union(self, rhs: Rhs) -> Self::Output
+    where
+        Self: OrderedHSet;
+}
+
+impl<H, T> Union<Cons<LabeledValue<H>, T>> for Nil
+where
+    H: Label,
+    T: OrderedHSet,
+{
+    type Output = Cons<LabeledValue<H>, T>;
+
+    fn union(self, rhs: Cons<LabeledValue<H>, T>) -> Self::Output {
+        rhs
+    }
+}
+
+impl<H, T> Union<Nil> for Cons<LabeledValue<H>, T>
+    where
+        H: Label,
+        T: OrderedHSet,
+{
+    type Output = Cons<LabeledValue<H>, T>;
+
+    fn union(self, _rhs: Nil) -> Self::Output {
+        self
+    }
+}
+
+impl Union<Nil> for Nil
+{
+    type Output = Nil;
+
+    fn union(self, rhs: Nil) -> Self::Output {
+        rhs
+    }
+}
+
+impl<H1, T1, H2> Union<Cons<LabeledValue<H2>, Nil>> for Cons<LabeledValue<H1>, T1>
+where
+    H1: Label,
+    T1: OrderedHSet,
+    H2: Label,
+    Self: Member<H2, Output=False>,
+{
+    type Output = Cons<LabeledValue<H2>, Cons<LabeledValue<H1>, T1>>;
+
+    fn union(self, rhs: Cons<LabeledValue<H2>, Nil>) -> Self::Output {
+        self.prepend(rhs.head)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::ordered_set::OrderedHSet;
+    use crate::ordered_set::{OrderedHSet, Union};
     use crate::*;
 
     #[test]
@@ -34,13 +89,20 @@ mod tests {
         #[label(type=u8, crate=crate)]
         struct ShelfId;
 
+        #[label(type=String, crate=crate)]
+        struct ShelfName;
+
         let nil = Nil {};
         let name = LabeledValue::<ProductName>::new("Shampoo".to_string());
         let product_id = LabeledValue::<ProductId>::new(10);
         let shelf_id = LabeledValue::<ShelfId>::new(10);
-        let _ordered_set = nil
+        let shelf_name = LabeledValue::<ShelfName>::new("Home".to_string());
+        let ordered_set = nil.clone()
             .prepend(name)
             .prepend(product_id)
             .prepend(shelf_id);
+        let singleton = nil.prepend(shelf_name);
+
+        ordered_set.union(singleton);
     }
 }
